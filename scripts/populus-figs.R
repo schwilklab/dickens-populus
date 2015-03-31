b8a4398d3fc6dd198fb9235ffb0cbd7e2eb95348
Dylan W. Schwilk <dylan@schwilk.org>
2015-03-31 16:50:06 -0500

    Merge waypoints with dbh data

--- scripts/populus-figs.R
+++ scripts/populus-figs.R
@@ -2,7 +2,10 @@
 library(ggplot2)
 
 populus <- read.csv("../data/populus.csv")
-
+waypoints <- read.csv("../data/waypoints.csv", stringsAsFactors=FALSE)
+populus <- merge(populus, waypoints)
 
 ggplot(populus, aes(DBH)) + geom_histogram()
 ggsave("../results/size-structure.pdf")
+
+ggplot(populus, aes(lon, DBH)) + geom_point()
